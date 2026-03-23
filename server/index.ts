4c9971c4f4f986afa7368487aef981cf64040fb8
emekabond <emeka@getatlasos.com>
2026-03-23 03:32:12 -0400

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -26,3 +26,6 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
